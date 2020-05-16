5386eecdaa673526fafd33064c2ea5a440de435f
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-05-16 00:22:35 -0400

    edit comments

--- src/main/Entry.java
+++ src/main/Entry.java
@@ -3,8 +3,8 @@ package main;
 /**
  * The {@code Entry} interface abstracts the association between a key and a value.
  *
- * @param <K> the type of key
- * @param <V> the type of value
+ * @param <K> the type of key to this {@code Entry}
+ * @param <V> the type of value of this {@code Entry}
  */
 public interface Entry<K, V> {
 
