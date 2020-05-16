7a4efbc9d0d1af308aa7161af4908935f62e9dd9
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-05-16 11:34:09 -0400

    initial commit

---
+++ src/main/AbstractMap.java
@@ -0,0 +1,79 @@
+package main;
+
+import java.io.Serializable;
+
+/**
+ * The {@code AbstractMap} class is the base class from which all {@code Map} implementations shall be derived.
+ *
+ * @param <K> the type of key that maps to values
+ * @param <V> the type of mapped values
+ * @author Jeff Wilgus
+ */
+public abstract class AbstractMap<K, V> implements Map<K, V>, Serializable {
+
+	int size;
+
+	@Override
+	public void clear() {
+		init();
+	}
+
+	/**
+	 * Puts this {@code AbstractMap} into its native state.
+	 */
+	abstract void init();
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public V replace(final K key, final V value) {
+		if (contains(key)) {
+			return put(key, value);
+		}
+		return null;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (!(obj instanceof AbstractMap)) {
+			return false;
+		}
+		return entrySet().equals(((AbstractMap<?, ?>) obj).entrySet());
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 0;
+		for (Entry<K, V> entry : entrySet()) {
+			result += entry.hashCode();
+		}
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		int index = 0;
+		StringBuilder builder = new StringBuilder("[");
+		for (Entry<K, V> entry : entrySet()) {
+			builder.append(entry);
+			if (++index < size) {
+				builder.append(", ");
+			}
+		}
+		return builder.append("]").toString();
+	}
+
+	private static final long serialVersionUID = -1517329747185431297L;
+
+}
