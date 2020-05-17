6563376f0405479467d6a8b59ac66978090e30a9
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-05-17 11:02:22 -0400

    initial commit

---
+++ src/main/TreeSet.java
@@ -0,0 +1,82 @@
+package main;
+
+import java.util.Comparator;
+
+/**
+ * The {@code TreeSet} class is a red-black tree implementation of the {@code Set} interface. Elements are sorted on the
+ * order induced by a supplied {@code Comparator}, or their natural ordering if none is supplied. Elements may be {@code
+ * null}.
+ *
+ * @param <E> the type of element in this {@code TreeSet}
+ * @author Jeff Wilgus
+ */
+public class TreeSet<E> extends AbstractSet<E> implements OrderedSet<E> {
+
+	/**
+	 * Constructs a new {@code TreeSet} object.
+	 */
+	public TreeSet() {
+		map = new TreeMap<>();
+	}
+
+	/**
+	 * Constructs a new {@code TreeSet} object sorted on the order induced by the specified {@code Comparator}.
+	 *
+	 * @param comp the specified {@code Comparator}
+	 */
+	public TreeSet(Comparator<E> comp) {
+		map = new TreeMap<>(comp);
+	}
+
+	@Override
+	void init() {
+		map().init();
+	}
+
+	@Override
+	public E removeFirst() {
+		return map().removeFirst().getKey();
+	}
+
+	@Override
+	public E removeLast() {
+		return map().removeLast().getKey();
+	}
+
+	@Override
+	public E removePrevious(final E element) {
+		return map().removePrevious(element).getKey();
+	}
+
+	@Override
+	public E removeNext(final E element) {
+		return map().removeNext(element).getKey();
+	}
+
+	@Override
+	public E getFirst() {
+		return map().getFirst().getKey();
+	}
+
+	@Override
+	public E getLast() {
+		return map().getLast().getKey();
+	}
+
+	@Override
+	public E getPrevious(final E element) {
+		return map().getPrevious(element).getKey();
+	}
+
+	@Override
+	public E getNext(final E element) {
+		return map().getNext(element).getKey();
+	}
+
+	private TreeMap<E, Void> map() {
+		return (TreeMap<E, Void>) map;
+	}
+
+	private static final long serialVersionUID = 7605404853438032617L;
+
+}
