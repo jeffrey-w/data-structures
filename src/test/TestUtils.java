723e12dfe71cbd0e57a1e3fa368e7e21ba96de89
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-05-24 19:42:26 -0400

    add SIZE, VALUE, and PREV

--- src/test/TestUtils.java
+++ src/test/TestUtils.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 
 final class TestUtils {
 
+	static final int SIZE = 2 << 20;
 	static final String PATH = "./output/";
+	static final TestObject VALUE = TestObject.random();
+	static Object PREV;
 
 	static void openTestDir() throws IOException {
 		File dir = new File(PATH);
-		if(!dir.exists() && !dir.mkdir()) {
+		if (!dir.exists() && !dir.mkdir()) {
 			throw new IOException();
 		}
 	}
