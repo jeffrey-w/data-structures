0465dc7e28629355d5d3c93312f3d119b55a7c99
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-05-24 19:41:56 -0400

    remove SIZE, PRESENT, and PREV; add writeObject() and readObject()

--- src/test/HashMapTest.java
+++ src/test/HashMapTest.java
@@ -1,27 +1,31 @@
 package test;
 
 import main.HashMap;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
+import java.io.*;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static test.TestUtils.*;
 
+@TestMethodOrder(OrderAnnotation.class)
 class HashMapTest {
 
-	private static final int SIZE = 2 << 10;
-	private static final TestObject PRESENT = TestObject.random();
-	private static HashMap<TestObject, TestObject> PREV;
-
 	private HashMap<TestObject, TestObject> empty, full;
+
+	@BeforeAll
+	static void beforeAll() throws IOException {
+		openTestDir();
+	}
 
 	@BeforeEach
 	void setUp() {
 		empty = new HashMap<>();
 		full = new HashMap<>();
-		for(int i = 0; i < SIZE; i++) {
-			full.put(new TestObject(i), PRESENT);
+		for (int i = 0; i < SIZE; i++) {
+			full.put(new TestObject(i), VALUE);
 		}
 	}
 
@@ -29,7 +33,7 @@ class HashMapTest {
 	void contains() {
 		TestObject key = new TestObject(SIZE);
 		assertFalse(full.contains(key));
-		full.put(key, PRESENT);
+		full.put(key, VALUE);
 		assertTrue(full.contains(key));
 	}
 
@@ -37,21 +41,21 @@ class HashMapTest {
 	void put() {
 		TestObject key = new TestObject(SIZE - 1);
 		assertNull(empty.put(key, TestObject.random()));
-		assertEquals(PRESENT, full.put(key, TestObject.random()));
+		assertEquals(VALUE, full.put(key, TestObject.random()));
 	}
 
 	@Test
 	void remove() {
 		assertThrows(IllegalStateException.class, () -> empty.remove(null));
 		assertThrows(NoSuchElementException.class, () -> full.remove(null));
-		assertEquals(PRESENT, full.remove(new TestObject(SIZE - 1)));
+		assertEquals(VALUE, full.remove(new TestObject(SIZE - 1)));
 	}
 
 	@Test
 	void get() {
 		assertThrows(IllegalStateException.class, () -> empty.get(null));
 		assertThrows(NoSuchElementException.class, () -> full.get(null));
-		assertEquals(PRESENT, full.get(new TestObject(SIZE - 1)));
+		assertEquals(VALUE, full.get(new TestObject(SIZE - 1)));
 	}
 
 	@Test
@@ -65,4 +69,30 @@ class HashMapTest {
 		assertThrows(NoSuchElementException.class, iterator::next);
 		assertThrows(IllegalStateException.class, iterator::remove);
 	}
+
+	@Test
+	@Order(1)
+	void writeObject() {
+		try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(PATH + "HashMap.dat"))) {
+			out.writeObject(full);
+			PREV = full;
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Test
+	@Order(2)
+	void readObject() {
+		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(PATH + "HashMap.dat"))) {
+			@SuppressWarnings("unchecked")
+			HashMap<TestObject, TestObject> map = (HashMap<TestObject, TestObject>) in.readObject();
+			assertEquals(PREV, map);
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
 }
