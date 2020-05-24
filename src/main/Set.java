0fd7d631b79004a7e47cbeb184e657488b26483b
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-05-24 13:10:59 -0400

    add comments

--- src/main/Set.java
+++ src/main/Set.java
@@ -2,6 +2,8 @@ package main;
 
 import java.util.NoSuchElementException;
 
+// TODO specify parameters as final
+
 /**
  * The {@code Set} interface specifies operations on a collection of unique elements.
  *
