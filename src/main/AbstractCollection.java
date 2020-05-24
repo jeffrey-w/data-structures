dd3fa5274c40c2bd5c7172e28019f17d065ea5ca
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-05-24 19:35:33 -0400

    remove validatePosition()

--- src/main/AbstractCollection.java
+++ src/main/AbstractCollection.java
@@ -60,29 +60,6 @@ public abstract class AbstractCollection<E> implements Collection<E>, Serializab
 		return elements;
 	}
 
-	/**
-	 * Ensures that the specified {@code Position} is valid insofar as it belongs to this {@code AbstractCollection}.
-	 *
-	 * @param position the specified {@code Position}
-	 * @param type the expected type of the specified {@code Position}
-	 * @return the specified {@code Position} cast to the specified type of {@code AbstractPosition}
-	 * @throws IllegalArgumentException if the specified {@code Position} is not of the specified {@code type} or it is
-	 * not owned by this {@code AbstractCollection}
-	 * @throws IllegalStateException if this {@code AbstractCollection} is empty
-	 * @throws NullPointerException if the specified {@code Position} is {@code null}
-	 */
-	@SuppressWarnings({"rawtypes", "unchecked"})
-	AbstractPosition<E> validatePosition(Position<E> position, Class<? extends AbstractPosition> type) {
-		if (isEmpty()) {
-			throw new IllegalStateException();
-		}
-		AbstractPosition<E> abstractPosition;
-		if (!(type.isInstance(position)) || !(abstractPosition = type.cast(position)).isOwnedBy(this)) {
-			throw new IllegalArgumentException();
-		}
-		return abstractPosition;
-	}
-
 	private static final long serialVersionUID = 1294951821974395815L;
 
 }
