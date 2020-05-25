aaecc8b5462be88f024b5f1cb5cf01afbaee57ef
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-05-25 18:05:44 -0400

    add comments; modify openTestDir()

--- src/test/TestUtils.java
+++ src/test/TestUtils.java
@@ -5,13 +5,33 @@ import java.io.IOException;
 
 final class TestUtils {
 
-	static final int SIZE = 2 << 20;
+	/**
+	 * The number of elements to insert into a container.
+	 */
+	static final int SIZE = 0x10000;
+
+	/**
+	 * The path to a directory for temporary test files.
+	 */
 	static final String PATH = "./output/";
+
+	/**
+	 * A common {@code TestObject} to store in containers.
+	 */
 	static final TestObject VALUE = TestObject.random();
+
+	/**
+	 * A store for inter-test objects.
+	 */
 	static Object PREV;
 
-	static void openTestDir() throws IOException {
-		File dir = new File(PATH);
+	/**
+	 * Attempts to open (or create) a temporary directory for test output.
+	 *
+	 * @throws IOException if the temporary directory could not be opened (or created)
+	 */
+	static void openTestDir(String path) throws IOException {
+		File dir = new File(path);
 		if (!dir.exists() && !dir.mkdir()) {
 			throw new IOException();
 		}
