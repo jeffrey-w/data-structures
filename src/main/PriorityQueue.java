0478b5bb5fc8082354b14182e01d2ee65b74bb3f
jeff <jeff@git.com>
2020-05-28 18:58:34 -0400

    implement methods

--- src/main/PriorityQueue.java
+++ src/main/PriorityQueue.java
@@ -1,4 +1,100 @@
 package main;
 
-public class PriorityQueue {
+import util.DefaultComparator;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class PriorityQueue<E> extends AbstractListAdaptor<E> implements Queue<E> {
+
+    private final Comparator<E> comp;
+
+    public PriorityQueue() {
+        this(new DefaultComparator<>());
+    }
+
+    public PriorityQueue(Comparator<E> comp) {
+        data = new ArrayList<>();
+        this.comp = Objects.requireNonNull(comp);
+    }
+
+    @Override
+    public void enqueue(final E element) {
+        data.addLast(element);
+        upheap(size() - 1);
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException();
+        }
+        E result = data.getFirst();
+        swap(0, size() - 1);
+        downheap(0);
+        return result;
+    }
+
+    private void upheap(int index) {
+        while (index > 0) {
+            int parent = parent(index);
+            if (comp.compare(data.get(index), data.get((parent))) > -1) {
+                break;
+            }
+            swap(index, parent);
+            index = parent;
+        }
+    }
+
+    private void downheap(int index) {
+        while (hasLeft(index)) {
+            int left, small;
+            left = small = left(index);
+            if (hasRight(index)) {
+                int right = right(index);
+                if (comp.compare(data.get(left), data.get(right)) > 0) {
+                    small = right;
+                }
+            }
+            if (comp.compare(data.get(small), data.get(index)) > -1) {
+                break;
+            }
+            swap(index, small);
+            index = small;
+        }
+    }
+
+    private void swap(int i, int j) {
+        E temp = data.get(i);
+        data.set(i, data.get(j));
+        data.set(j, temp);
+    }
+
+    private int parent(int index) {
+        return (index - 1) << 1;
+    }
+
+    private int left(int index) {
+        return index << 1 + 1;
+    }
+
+    private int right(int index) {
+        return index << 1 + 2;
+    }
+
+    private boolean hasLeft(int index) {
+        return left(index) < size();
+    }
+
+    private boolean hasRight(int index) {
+        return right(index) < size();
+    }
+
+    @Override
+    public E first() {
+        return data.getFirst();
+    }
+
+    private static final long serialVersionUID = -2669565621428618944L;
+
 }
