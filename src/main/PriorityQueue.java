ce31d92da5a28fe7ce1555e48374878bfc3445f5
jeffrey-w <jeff@git.com>
2020-05-29 17:41:46 -0400

    Inherit from AbstractQueue; modify sort(); add comments; formatting

--- src/main/PriorityQueue.java
+++ src/main/PriorityQueue.java
@@ -10,19 +10,41 @@ import java.util.Objects;
 
 import static util.Common.validateObject;
 
-public class PriorityQueue<E> extends AbstractListAdaptor<E> implements Queue<E>, Sortable<E> {
+/**
+ * The {@code PriorityQueue} class is an implementation of the {@code Queue} interface for which a prescribed order
+ * on the elements is maintained. At all times, the element at the front of this {@code Queue} is the minimum element.
+ * Insertion and removal operations perform in logarithmic time. Null elements are permitted, but must be mutually
+ * comparable with the other elements in this {@code PriorityQueue}.
+ *
+ * @param <E> the type of element in this {@code PriorityQueue}
+ * @author Jeff Wilgus
+ */
+public class PriorityQueue<E> extends AbstractQueue<E> implements Sortable<E> {
 
     private Comparator<E> comp;
 
+    /**
+     * Constructs a new {@code PriorityQueue} object.
+     */
     public PriorityQueue() {
         this(new DefaultComparator<>());
     }
 
+    /**
+     * Constructs a new {@code PriorityQueue} object sorted on the order induced by the specified {@code Comparator}.
+     *
+     * @param comp the specified {@code Comparator}
+     * @throws NullPointerException if the specified {@code Comparator} is {@code null}
+     */
     public PriorityQueue(final Comparator<E> comp) {
         data = new ArrayList<>();
         this.comp = Objects.requireNonNull(comp);
     }
 
+    /**
+     * {@inheritDoc} The specified {@code element} is promoted to the appropriate position in this {@code
+     * PriorityQueue} as determined by its relative order among other elements.
+     */
     @Override
     public void enqueue(final E element) {
         data.addLast(element);
@@ -69,22 +91,16 @@ public class PriorityQueue<E> extends AbstractListAdaptor<E> implements Queue<E>
         }
     }
 
-    private void swap(int i, int j) {
-        E temp = data.get(i);
-        data.set(i, data.get(j));
-        data.set(j, temp);
-    }
-
     private int parent(int index) {
         return (index - 1) >> 1;
     }
 
     private int left(int index) {
-        return index << 1 + 1;
+        return (index << 1) + 1;
     }
 
     private int right(int index) {
-        return index << 1 + 2;
+        return (index << 1) + 2;
     }
 
     private boolean hasLeft(int index) {
@@ -95,6 +111,12 @@ public class PriorityQueue<E> extends AbstractListAdaptor<E> implements Queue<E>
         return right(index) < size();
     }
 
+    private void swap(int i, int j) {
+        E temp = data.get(i);
+        data.set(i, data.get(j));
+        data.set(j, temp);
+    }
+
     @Override
     public E first() {
         return data.getFirst();
@@ -102,8 +124,12 @@ public class PriorityQueue<E> extends AbstractListAdaptor<E> implements Queue<E>
 
     @Override
     public void sort(final Comparator<E> comp) {
-        data.sort(comp);
-        this.comp = comp;
+        E[] elements = data.toArray();
+        this.comp = Objects.requireNonNullElseGet(comp, DefaultComparator::new);
+        data.init();
+        for (E element : elements) {
+            enqueue(element);
+        }
     }
 
     private static final long serialVersionUID = -2669565621428618944L;
