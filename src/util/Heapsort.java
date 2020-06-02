b2f26eeb8708d52da592e4a76f00a7350f827947
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-06-02 18:58:49 -0400

    modify sort(); remove heap

--- src/util/Heapsort.java
+++ src/util/Heapsort.java
@@ -4,24 +4,17 @@ import main.PriorityQueue;
 
 import java.util.Comparator;
 
-public class Heapsort<E> {
+public class Heapsort<E> extends AbstractSort<E> {
 
-    private final PriorityQueue<E> heap;
-
-    public Heapsort(Comparator<E> comp) {
-        if(comp == null) {
-            heap = new PriorityQueue<>();
-        } else {
-            heap = new PriorityQueue<>(comp);
-        }
-    }
-
-    public void sort(E[] array) {
-        for (E element : array) {
+    public void sort(E[] elements, final Comparator<E> comp) {
+        setElements(elements);
+        setComp(comp);
+        PriorityQueue<E> heap = new PriorityQueue<>(this.comp);
+        for (E element : elements) {
             heap.enqueue(element);
         }
-        for (int i = 0; i < array.length; i++) {
-            array[i] = heap.dequeue();
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = heap.dequeue();
         }
     }
 
